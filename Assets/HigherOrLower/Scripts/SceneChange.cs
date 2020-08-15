9dd50dffe99023ca68668813ff86d244f6759c50
dr-matt-smith <matt.smith@itb.ie>
2020-08-15 13:16:51 +0100

    added files to project

---
+++ Assets/HigherOrLower/Scripts/SceneChange.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+	const int MAIN_MENU = 0;
+	const int GAME_PLAYING = 1;
+	const int GAME_WON = 2;
+	const int GAME_LOST = 3;
+	
+	public void GotoScene(int sceneNumber){
+		SceneManager.LoadScene(sceneNumber);
+
+	}
+}
